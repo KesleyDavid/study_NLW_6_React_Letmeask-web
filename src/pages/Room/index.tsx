8ca1b4f32533a6b90d259f97bd1b05e2466116fd
Kesley David <kesleygti@gmail.com>
2021-06-23 13:05:42 -0300

    feat: page sale (tmp)

--- src/pages/Room/index.tsx
+++ src/pages/Room/index.tsx
@@ -1,9 +1,43 @@
-import { Container } from './styles';
+import { useParams } from 'react-router-dom';
+
+import imgLogo from '../../assets/images/logo.svg';
+
+import { Button } from '../../components/Button';
+import { RoomCode } from '../../components/RoomCode';
+
+import { Container, Content, Title, Form } from './styles';
+
+type RoomParams = {
+  id: string;
+}
 
 export default function Room() {
+  const params = useParams<RoomParams>();
+
   return (
     <Container>
-      <h1>Sala</h1>
+      <header>
+        <Content>
+          <img src={imgLogo} alt="Letmeask" />
+          <RoomCode code={params.id} />
+        </Content>
+      </header>
+      <main>
+        <Title>
+          <h1>Sala React</h1>
+          <span>4 perguntas</span>
+        </Title>
+
+        <Form>
+          <textarea
+            placeholder="O que você quer perguntar?"
+          />
+          <div>
+            <span>Para enviar uma pergunta, <button>faça seu login</button>.</span>
+            <Button type="submit">Enviar pergunta</Button>
+          </div>
+        </Form>
+      </main>
     </Container>
   )
 }
--- src/pages/Room/styles.ts
+++ src/pages/Room/styles.ts
@@ -1,5 +1,82 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
+  header {
+    padding: 1.5rem;
+    border-bottom: 1px solid #e2e2e2;
+  }
+
+  main {
+    max-width: 800px;
+    margin: 0 auto;
+  }
+`;
+
+export const Content = styled.div`
+  max-width: 1120px;
+  margin: 0 auto;
   display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  img {
+    max-height: 45px;
+  }
+`;
+
+export const Title = styled.div`
+  margin: 2rem 0 1.5rem;
+  display: flex;
+  align-items: center;
+
+  h1 {
+    font-family: 'Poppins', sans-serif;
+    font-size: 1.5rem;
+    color: var(--black);
+  }
+
+  span {
+    margin-left: 1rem;
+    background: var(--pink-dark);
+    border-radius: 9999px;
+    padding: 8px 16px;
+    color: var(--white);
+    font-size: 0.87rem;
+  }
+`;
+
+export const Form = styled.form`
+  textarea {
+    width: 100%;
+    border: 0;
+    padding: 1rem;
+    border-radius: 8px;
+    background: var(--white-background);
+    box-shadow: 0 2px 12px rgba(0, 0, 0, 0.04);
+    resize: vertical;
+    min-height: 130px;
+  }
+
+  div {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    margin-top: 1rem;
+
+    span {
+      font-size: 0.87em;
+      color: var(--gray-dark);
+      font-weight: 500;
+
+      button {
+        background: transparent;
+        border: 0;
+        color: var(--purple);
+        text-decoration: underline;
+        font-size: 0.87rem;
+        font-weight: 500;
+        cursor: pointer;
+      }
+    }
+  }
 `;
